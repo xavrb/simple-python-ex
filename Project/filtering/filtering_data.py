1078fd2ff7b820a20f2f4d4a932fbc3113c90da9
Javier Mejia <javier.mejia@pm.me>
2021-04-11 22:55:11 -0500

    added filtering using comprehension lists filter and map over a list of dictionaries

---
+++ Project/filtering/filtering_data.py
@@ -0,0 +1,25 @@
+import ast 
+
+def run():
+    DATA = read_data_bulk("./data/names_langs.dat")
+    
+    # Using comprehension lists
+    all_python_devs = [worker["name"] for worker in DATA if worker["language"]== "python"]
+    print(all_python_devs)
+    all_platzi_workers = [worker["name"]for worker in DATA if worker["organization"] == "Platzi"]
+    print(all_platzi_workers)
+    
+    # using filter
+    all_adults = list(map(lambda adult: adult["name"], list(filter(lambda adult: adult["age"] >= 18, DATA))))
+    print(all_adults)
+
+
+def read_data_bulk(data_set):
+    with open(data_set) as file:
+        data_list =ast.literal_eval(file.read())
+    return data_list
+    
+
+
+if __name__== "__main__":
+    run()
