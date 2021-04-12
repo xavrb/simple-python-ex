272dcd17ea58d176bff03bcd7337f28be6d0e1f8
Javier Mejia <javier.mejia@pm.me>
2021-04-11 20:31:25 -0500

    added squared from 1 to 1000

--- dictionaries/dict_comprehension.py
+++ dictionaries/dict_comprehension.py
@@ -13,12 +13,16 @@ def cubeItDict():
 def cubeItDiv3():
     return {i: math.pow(i,3) for i in range(1,101) if i%3 != 0}
 
+def sqIt():
+    return {i: math.pow(i,1/2) for i in range(1,1001)}
+
+
 def run():
     print(f"{cubeItList()}\n")
     print(f"{cubeItDict()}\n")
 
     print(f"Non divisible by 3: {cubeItDiv3()}\n")
 
-
+    print(f"Squares: {sqIt()}\n")
 if __name__ == "__main__":
     run()
